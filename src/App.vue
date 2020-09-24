2247f5859a46b2b647fcc349b63828eb4e55fd44
Cabrini Alves <cabrinireis@gmail.com>
2020-09-23 22:27:01 -0300

    enviando para github

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,38 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div id="app" class="container-fluid">
+    <transition name="fade" mode="out-in">
+      <router-view/>
+    </transition>
   </div>
 </template>
 
-<style>
+<style lang="scss">
+@import './assets/styles/variables';
+@import './assets/reset-css/index.css';
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #fff;
+  font-size: $textos;
+  padding: 0;
+  overflow: hidden;
+  .conteudo {
+    background-image: linear-gradient($gradiente) ;
+    height: 70vh;
+  }
+  .nav {
+    height: 30vh;
+  }
+}
+// transition
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+  transform: translate(2em);
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.fade-enter-active, .fade-leave-active {
+   transition: all .3s ease;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home row">
+    <Pesquisar msg="Localizador de Emprezas"/>
+    <div class="container-fluid conteudo col-12" >
+      <div class="row cont-home">
+        <figure class="col-12 m-auto">
+          <img src="../assets/search.svg" alt="Kiwi standing on oval">
+          <dir class="col-12">
+            <span> Faça aqui a pesquisa de sua empreza</span>
+          </dir>
+        </figure>
+      </div>
+      <div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Pesquisar from '@/components/Pesquisar.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Pesquisar
   }
 }
 </script>
+<style lang="scss" scoped>
+@import '../assets/styles/variables' ;
+.cont-home {
+  height: 100%;
+}
+  img {
+    max-width: 15%;
+    height: auto;
+  }
+</style>
